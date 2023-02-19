4a7f018955904ebca033c4ee3807cc4b4722cb41
william-italia <williamitalia70@outlook.com>
2023-02-18 23:45:28 -0300

    menu-mobile legado

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+var menuMobile = document.querySelectorAll("toggler");
+var showMenu = document.getElementsByClassName('menu-content')
+
+toggle.addEventListener('click',  function() {
+    let menumobile = document.querySelector('.menu-content')
+    menumobile.classList.add('menu-content')
+})
